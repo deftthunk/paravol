4af743f80933fb5b7771573a69a36557c092c6e1
bruce <35551306+deftthunk@users.noreply.github.com>
2018-10-16 00:40:53 -0400

    changed yaml parsing to now use Go reflection in iteration instead of unmarshaling into predefined structs. should make config more flexible and futureproof; updated yaml example to indicate double quoting empty fields. may change in future

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
   "fmt"
   "os"
-  "strings"
+//  "strings"
   "reflect"
-  "path/filepath"
+//  "path/filepath"
   "io/ioutil"
   "gopkg.in/yaml.v2"
   "log"
@@ -21,102 +21,15 @@ var pf = fmt.Printf
 var sp = fmt.Sprint
 
 
-type Plugin struct {
-  PluginName      string    `yaml:"plugin"`
-  Pid             string    `yaml:"pid,omitempty"`
-  Address         string    `yaml:"address,omitempty"`
-  HiveOffset      string    `yaml:"hive-offset,omitempty"`
-  Offset          string    `yaml:"offset,omitempty"`
-  Name            string    `yaml:"name,omitempty"`
-  Fix             string    `yaml:"fix,omitempty"`
-  Quick           string    `yaml:"quick,omitempty"`
-  NoWhitelist     string    `yaml:"no-whitelist,omitempty"`
-  SkipKernel      string    `yaml:"skip-kernel,omitempty"`
-  SkipProcess     string    `yaml:"skip-process,omitempty"`
-  Virtual         string    `yaml:"virtual,omitempty"`
-  ShowUnallocated string    `yaml:"show-unallocated,omitempty"`
-  StartAddress    string    `yaml:"start-address,omitempty"`
-  Length          string    `yaml:"length,omitempty"`
-  SysOffset       string    `yaml:"sys-offset,omitempty"`
-  SecOffset       string    `yaml:"sec-offset,omitempty"`
-  MaxHistory      string    `yaml:"max-history,omitempty"`
-  PhysicalOffset  string    `yaml:"physical-offset,omitempty"`
-  HistoryBuffers  string    `yaml:"history-buffers,omitempty"`
-  DumpDir         string    `yaml:"dump-dir,omitempty"`
-  Regex           string    `yaml:"regex,omitempty"`
-  IgnoreCase      string    `yaml:"ignore-case,omitempty"`
-  Base            string    `yaml:"base,omitempty"`
-  Addr            string    `yaml:"addr,omitempty"`
-  Ssl             string    `yaml:"ssl,omitempty"`
-  Physical        string    `yaml:"physical,omitempty"`
-  PhysOffset      string    `yaml:"physoffset,omitempty"`
-  SummaryFile     string    `yaml:"summary-file,omitempty"`
-  Unsafe          string    `yaml:"unsafe,omitempty"`
-  Filter          []string  `yaml:"filter,omitempty"`
-  Silent          string    `yaml:"silent,omitempty"`
-  ObjectType      []string  `yaml:"object-type,omitempty"`
-  SamOffset       string    `yaml:"sam-offset,omitempty"`
-  BlockSize       string    `yaml:"blocksize,omitempty"`
-  OutputImage     string    `yaml:"output-image,omitempty"`
-  Count           string    `yaml:"count,omitempty"`
-  Size            string    `yaml:"size,omitempty"`
-  MaxSize         string    `yaml:"max-size,omitempty"`
-  Refined         string    `yaml:"refined,omitempty"`
-  Hex             string    `yaml:"hex,omitempty"`
-  Hash            string    `yaml:"hash,omitempty"`
-  FullHash        string    `yaml:"fullhash,omitempty"`
-  Disoffset       string    `yaml:"disoffset,omitempty"`
-  NoCheck         string    `yaml:"nocheck,omitempty"`
-  Disk            string    `yaml:"disk,omitempty"`
-  MaxDistance     string    `yaml:"maxdistance,omitempty"`
-  ZeroStart       string    `yaml:"zerostart,omitempty"`
-  Machine         string    `yaml:"machine,omitempty"`
-  DebugOut        string    `yaml:"debugout,omitempty"`
-  Memory          string    `yaml:"memory,omitempty"`
-  Tag             string    `yaml:"tag,omitempty"`
-  MinSize         string    `yaml:"min-size,omitempty"`
-  Paged           string    `yaml:"paged,omitempty"`
-  Key             string    `yaml:"key,omitempty"`
-  ApplyRules      string    `yaml:"apply-rules,omitempty"`
-  StringFile      string    `yaml:"string-file,omitempty"`
-  Scan            string    `yaml:"scan,omitempty"`
-  LookupPid       string    `yaml:"lookup-pid,omitempty"`
-  ListTags        string    `yaml:"listtags,omitempty"`
-  Hive            string    `yaml:"hive,omitempty"`
-  User            string    `yaml:"user,omitempty"`
-  Type            []string  `yaml:"type,omitempty"`
-  ListHead        string    `yaml:"listhead,omitempty"`
-  Free            string    `yaml:"free,omitempty"`
-  All             string    `yaml:"all,omitempty"`
-  Case            string    `yaml:"case,omitempty"`
-  Kernel          string    `yaml:"kernel,omitempty"`
-  Wide            string    `yaml:"wide,omitempty"`
-  YaraRules       string    `yaml:"yara-rules,omitempty"`
-  YaraFile        string    `yaml:"yara-file,omitempty"`
-  Reverse         string    `yaml:"reverse,omitempty"`
-}
-
-type Config struct {
-  Threads     int       `yaml:"threads"`
-  Profile     string    `yaml:"profile"`
-  SubFolders  string    `yaml:"subfolders"`
-  Memdumps    string    `yaml:"memdumps"`
-  OutPath     string    `yaml:"output"`
-  ProcPid     string    `yaml:"proc_pid"`
-  Modules     []Plugin    `yaml:"plugins"`
-}
-
-
 /*
   grab YAML file and decode into structs
 */
-func input() Config {
-  var argFail string = "Missing path to config.yaml"
-  cfg := Config{}
+func input() {
+  var cfgMap map[string]interface{}
 
   // count and parse CLI args
   if len(os.Args) < 1 {
-    pl(argFail)
+    pl("Missing path to config.yaml")
     os.Exit(1)
   }
 
@@ -126,21 +39,63 @@ func input() Config {
     log.Fatal(err)
   }
 
-  // parse yaml into struct
-  decode_err := yaml.Unmarshal(cfgFile, &cfg)
+  // parse yaml into map
+  decode_err := yaml.Unmarshal(cfgFile, &cfgMap)
   if decode_err != nil {
     log.Fatalf("error: %v", decode_err)
   }
 
-  return cfg
+  convertToMap(cfgMap)
+//  return
 }
+
+func convertToMap(c map[string]interface{}) {
+  for k, v := range c {
+    if s, ok := v.(string); ok && s != "" {
+      pl("key: ", k, "val: ", s)
+    } else if s, ok := v.(int); ok {
+      pl("key: ", k, "val: ", s)
+    } else if s, ok := v.([]interface{}); ok {
+      test2(s)
+    } else {
+      pl("what am i: ", reflect.TypeOf(v))
+    }
+  }
+}
+
+func test2(c []interface{}) {
+  // iterate over "Plugins:" sub-config
+  for _, j := range c {
+    pl("ValueOf: ", reflect.ValueOf(j))
+    k := j.(map[interface{}]interface{})
+
+    for g, h := range k {
+      pl("I/V: ", reflect.ValueOf(g), reflect.ValueOf(h))
+    }
+  }
+}
+
+func convertToArray(c []interface{}) {
+  for i, v := range c {
+    if s, ok := v.(string); ok {
+      pl("idx: ", i, "val: ", s)
+    } else if s, ok := v.(int); ok {
+      pl("idx: ", i, "val: ", s)
+    } else {
+      pl("what is this: ", reflect.TypeOf(v))
+    }
+  }
+}
+
+
 
 
 /*
   returns 2D array of dumpfiles in the format of 
   [[filename, path] [filename, path]]
 */
-func (c Config) findDumps() [][]string {
+/*
+func findDumps() [][]string {
   // [[file, path], [file, path]]
   dumpFiles := make([][]string, 0)
 
@@ -182,13 +137,12 @@ func (c Config) findDumps() [][]string {
 
   return dumpFiles
 }
-
+*/
 
 /*
   Convert Plugin struct fields into an array of values. Array conversion
   allows for iteration and automatic building of command string using config
   values.
-*/
 func convertStruct(p Plugin) []interface{} {
   v := reflect.ValueOf(p)
   values := make([]interface{}, v.NumField())
@@ -200,12 +154,13 @@ func convertStruct(p Plugin) []interface{} {
   //pl(values)
   return values
 }
+*/
 
 
 /*
   build volatility command for each 'state' memory dump listed
 */
-func (c Config) buildCommands(dumpFiles [][]string) []string {
+func buildCommands(dumpFiles [][]string) []string {
   var commands []string
 
 /*  for _, pathArr := range dumpFiles {
@@ -218,11 +173,11 @@ func (c Config) buildCommands(dumpFiles [][]string) []string {
 */
 
     // per plugin command strings
-    for _, plugin := range c.Modules {
-      pluginArr := convertStruct(plugin)
-      pl(pluginArr)
+    //for _, plugin := range c.Modules {
+    //  pluginArr := convertStruct(plugin)
+    //  pl(pluginArr)
       //pl(plugin)
-    }
+    //}
 
 //  }
 
@@ -230,6 +185,7 @@ func (c Config) buildCommands(dumpFiles [][]string) []string {
 }
 
 
+// test struct-less yaml parsing
 func test() {
   var cfgMap map[string]interface{}
 
@@ -245,29 +201,22 @@ func test() {
     log.Fatalf("error: %v", decode_err)
   }
 
-  for i,j := range cfgMap {
-    if i == "plugins" {
-      pl("j: ", reflect.TypeOf(j))
-      arr, _ := j.([]map[string]string)
-      pl("arr", reflect.TypeOf(arr))
-      for w,x := range arr {
-        pl("w :", reflect.TypeOf(w))
-        pl("x :", reflect.TypeOf(x))
-        for y,z := range x {
-          pl("y - z: ", reflect.TypeOf(y), " - ", reflect.TypeOf(z))
-        }
-      }
+  for k, v := range cfgMap {
+    if s, ok := v.(string); ok && s != "" {
+      pl("key: ", k, "val: ", s)
+    } else if s, ok := v.(int); ok {
+      pl("key: ", k, "val: ", s)
     }
   }
 }
 
 
 func main() {
-  c := input()
+//  c := input()
 //  verifyConfig(c)
-  c.buildCommands(c.findDumps())
-
-  test()
+//  c.buildCommands(c.findDumps())
+input()
+//  test()
 }
 
 
